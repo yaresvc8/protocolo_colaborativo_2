562d1a3faee525dca6c1415242caca177f4ed060
yaresvc8 <yvilladiegoc@unicartagena.edu.co>
2025-09-14 15:20:15 -0500

    ejercicio 2 taller 8 (2)

---
+++ Estudiante.java
@@ -0,0 +1,17 @@
+package herencia;
+
+public class Estudiante extends Persona {
+    private String matricula;
+
+    public Estudiante(String nombre, int edad, String matricula) {
+        super(nombre, edad); 
+        this.matricula = matricula;
+    }
+
+    @Override
+    public void mostrarDetalles() {
+        super.mostrarDetalles(); 
+        System.out.println("Matrícula: " + matricula);
+    }
+}
+
