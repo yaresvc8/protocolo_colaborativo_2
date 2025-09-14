f316bb9fc7da96f4f09088fbac905f8dbcf6c5d6
yaresvc8 <yvilladiegoc@unicartagena.edu.co>
2025-09-14 15:19:37 -0500

    ejercicio 2 taller 8 (1)

---
+++ Persona.java
@@ -0,0 +1,17 @@
+package herencia;
+
+public class Persona {
+    protected String nombre;
+    protected int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void mostrarDetalles() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+}
+
