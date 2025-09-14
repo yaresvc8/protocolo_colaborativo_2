10c02c6f971d6d94ddba7e3f8434fcb989de55f5
yaresvc8 <yvilladiegoc@unicartagena.edu.co>
2025-09-14 15:40:23 -0500

    ejercicio 3 taller 8 (3)

--- pruebaHerencia.java
+++ pruebaHerencia.java
@@ -1,15 +1,17 @@
-package herencia;
+package empresa;
 
 public class pruebaHerencia {
     public static void main(String[] args) {
-        Estudiante est1 = new Estudiante("Carlos", 20, "2025A123");
-        Estudiante est2 = new Estudiante("María", 22, "2025B456");
+        
+        Empleado emp1 = new Empleado("Carlos Pérez", 1500000);
+        System.out.println("Detalles del empleado");
+        emp1.mostrarDetalles();
 
-        System.out.println("Detalles del estudiante 1");
-        est1.mostrarDetalles();
+        System.out.println();
 
-        System.out.println(" Detalles del estudiante 2");
-        est2.mostrarDetalles();
+        Gerente ger1 = new Gerente("Ana Gómez", 3500000, "Recursos Humanos");
+        System.out.println("Detalles del gerente");
+        ger1.mostrarDetalles();
     }
 }
 
