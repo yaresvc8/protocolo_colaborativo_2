86b483aad6e3320701b9c6103f83e4263e077abf
yaresvc8 <yvilladiegoc@unicartagena.edu.co>
2025-09-14 15:07:03 -0500

    ejercicio 1 taller 8 (1)

---
+++ Vehiculo.java
@@ -0,0 +1,18 @@
+package transporte;
+
+public class Vehiculo {
+    protected String marca;
+    protected int velocidadMaxima;
+
+    public Vehiculo(String marca, int velocidadMaxima) {
+        this.marca = marca;
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Velocidad máxima: " + velocidadMaxima + " km/h");
+    }
+}
+
+
