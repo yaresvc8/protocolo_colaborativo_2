4694423f8354820a7de28ccef1e5ebe26fea182c
yaresvc8 <yvilladiegoc@unicartagena.edu.co>
2025-09-14 20:18:54 -0500

    ejercico 4 taller 8 (1)

---
+++ herenciaMultiple.java
@@ -0,0 +1,18 @@
+class ClaseA {
+    public void metodoA() {
+        System.out.println("Método de ClaseA");
+    }
+}
+
+class ClaseB {
+    public void metodoB() {
+        System.out.println("Método de ClaseB");
+    }
+}
+
+public class herenciaMultiple {
+    public static void main(String[] args) {
+        System.out.println("Este ejemplo muestra que Java NO permite herencia múltiple de clases.");
+        System.out.println("Descomenta la línea de ClaseC para ver el error de compilación.");
+    }
+}
